db0c81656cd4065be5c7aa42991421252663549e
franktang <could.net@gmail.com>
2019-05-21 11:00:28 +0800

    modified to 1.13

--- co_echo_client/co_echo_cli.cpp
+++ co_echo_client/co_echo_cli.cpp
@@ -4,8 +4,8 @@
  *  Created on: Mar 9, 2019
  *      Author: frank
  */
-#include <asio/experimental/co_spawn.hpp>
-#include <asio/experimental/detached.hpp>
+#include <asio/co_spawn.hpp>
+#include <asio/detached.hpp>
 #include <asio/io_context.hpp>
 #include <asio/ip/tcp.hpp>
 #include <asio/signal_set.hpp>
@@ -15,26 +15,24 @@
 // clang++ -std=c++2a -DASIO_STAND_ALONE -fcoroutines-ts -stdlib=libc++ -I../asio/asio/include
 
 using asio::ip::tcp;
-using asio::experimental::co_spawn;
-using asio::experimental::detached;
-namespace this_coro = asio::experimental::this_coro;
-
-template <typename T>
-using awaitable = asio::experimental::awaitable<T, asio::io_context::executor_type>;
+using asio::co_spawn;
+using asio::detached;
+using asio::awaitable;
+using asio::use_awaitable;
+namespace this_coro = asio::this_coro;
 
 awaitable<void> Echo()
 {
-	auto token = co_await this_coro::token();
-	auto executor = co_await this_coro::executor();
-	tcp::socket socket(executor.context());
-	co_await socket.async_connect({tcp::v4(), 55555}, token); // 异步执行连接服务器
+	auto executor = co_await this_coro::executor;
+	tcp::socket socket(executor);
+	co_await socket.async_connect({tcp::v4(), 55555}, use_awaitable); // 异步执行连接服务器
 	for(int i = 0; i < 100; ++i) // echo 100次
 	{
 		char buff[256];
 		snprintf(buff, sizeof(buff), "hello %02d", i);
-		size_t n = co_await socket.async_send(asio::buffer(buff, strlen(buff)), token); // 异步写数据
+		size_t n = co_await socket.async_send(asio::buffer(buff, strlen(buff)), use_awaitable); // 异步写数据
 		//assert(n == strlen(buff));
-		n = co_await socket.async_receive(asio::buffer(buff), token); // 异步读数据
+		n = co_await socket.async_receive(asio::buffer(buff), use_awaitable); // 异步读数据
 		buff[n] = 0;
 		std::cout<<"received from server: "<<buff<<"\n";
 	}
