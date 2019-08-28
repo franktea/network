64814a2b07b29b7eb5482069ae6e3f74b72f6fab
franktang <could.net@gmail.com>
2019-08-28 17:32:40 +0800

    forget

---
+++ executor/basic_usage.cpp
@@ -0,0 +1,31 @@
+/*
+ * basic_usage.cpp
+ *
+ *  Created on: Aug 22, 2019
+ *      Author: frank
+ */
+#include <iostream>
+#include <chrono>
+#include "asio.hpp"
+
+int main()
+{
+    // 用法1：直接post
+    asio::post([](){ std::cout<<"hello world\n"; });
+
+    // 用法2：post到thread pool中去
+    asio::thread_pool pool;
+    asio::post(pool, [](){ std::cout<<"hello from pool\n"; });
+    pool.join();
+
+    // 用法3：使用future
+    std::future<int> f = asio::post(
+            asio::use_future([](){
+                return 99;
+            }));
+    std::cout<<"future get "<<f.get()<<"\n";
+
+    return 0;
+}
+
+
