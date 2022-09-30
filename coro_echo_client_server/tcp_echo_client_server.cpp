2067b04c77ecd83c44feb8c9780dfdc6e95f3f97
franktee <could.net@gmail.com>
2022-09-30 12:21:29 +0800

    tab

--- coro_echo_client_server/tcp_echo_client_server.cpp
+++ coro_echo_client_server/tcp_echo_client_server.cpp
@@ -18,25 +18,25 @@ tcp::endpoint ep{tcp::v4(), 10001};
 // 这时候只需要使用asio中对协程重载的运算符||，
 // 当客户端退出时，server也会退出。
 awaitable<void> Echo() {
-	auto client = []() -> awaitable<void> {
-		tcp_socket socket(co_await this_coro::executor);
-		co_await socket.async_connect(ep);
-		for(int i = 0; i < 10; ++i) {
-			std::string s = std::string("this is message ") + std::to_string(i);
-			std::string ret = co_await EchoClient(socket, s);
-			std::cout<<"client got: "<<ret<<"\n";
-		}
-	};
+    auto client = []() -> awaitable<void> {
+        tcp_socket socket(co_await this_coro::executor);
+        co_await socket.async_connect(ep);
+        for(int i = 0; i < 10; ++i) {
+            std::string s = std::string("this is message ") + std::to_string(i);
+            std::string ret = co_await EchoClient(socket, s);
+            std::cout<<"client got: "<<ret<<"\n";
+        }
+    };
 
-	// 其实将Listen放在前面也并不能100%保证server先启动，但是经过测试都ok，这样写并不严谨，
-	// 但是这不是这个例子演示的重点。
-	co_await (Listen(ep) || client());
+    // 其实将Listen放在前面也并不能100%保证server先启动，但是经过测试都ok，这样写并不严谨，
+    // 但是这不是这个例子演示的重点。
+    co_await (Listen(ep) || client());
 }
 
 int main() {
-	asio::io_context io_context(1);
-	asio::co_spawn(io_context, Echo(), detached);
-	io_context.run();
+    asio::io_context io_context(1);
+    asio::co_spawn(io_context, Echo(), detached);
+    io_context.run();
 }
 
 
