d0df68fd3766e65f8be7a6682d328a0a4f997c27
franktee <could.net@gmail.com>
2023-01-10 19:30:39 +0800

    helloworld

---
+++ grpc_coro/hello_world.cpp
@@ -0,0 +1,137 @@
+/**
+ * @file hello_world.cpp
+ * @author frank
+ * @brief 
+ * @version 0.1
+ * @date 2023-01-10
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <list>
+#include <memory>
+#include "helloworld.grpc.pb.h"
+#include "asio.hpp"
+#include "grpcpp/alarm.h"
+#include "grpcpp/grpcpp.h"
+
+template<class T, class... Args>
+auto allocate(Args&&... args)
+{
+    return new T{std::forward<Args>(args)...};
+}
+
+template<class T>
+void deallocate(T* t)
+{
+    delete t;
+}
+
+struct TypeErasedOperation
+{
+    using OnCompleteFunction = void (*)(TypeErasedOperation*, bool);
+    OnCompleteFunction on_complete;
+
+    void Complete(bool ok) { this->on_complete(this, ok); }
+
+    TypeErasedOperation(OnCompleteFunction on_complete) : on_complete(on_complete) {}
+};
+
+using QueuedOperations = std::list<TypeErasedOperation*>;
+
+template<class Function>
+struct Operation : TypeErasedOperation
+{
+    Function function;
+    Operation(Function f) : TypeErasedOperation{&Operation::DoComplete}, function(std::move(f)) {}
+    
+    static void DoComplete(TypeErasedOperation* base, bool ok)
+    {
+        auto* self = static_cast<Operation*>(base);
+        auto func = std::move(self->function);
+        deallocate(self);
+        func(ok);
+    }
+};
+
+struct GrpcContext : asio::execution_context
+{
+    struct executor_type;
+
+    std::unique_ptr<grpc::CompletionQueue> queue;
+    QueuedOperations queued_operations;
+    grpc::Alarm alarm;
+
+    explicit GrpcContext(std::unique_ptr<grpc::CompletionQueue> queue) : queue(std::move(queue)) {}
+
+    ~GrpcContext()
+    {
+        queue->Shutdown();
+        asio::execution_context::shutdown();
+        asio::execution_context::destroy();
+    }
+
+    executor_type get_executor() const noexcept;
+
+    static constexpr void* MAKER_TAG = nullptr;
+
+    void Run()
+    {
+        void* tag;
+        bool ok;
+        while(queue->Next(&tag, &ok))
+        {
+            if(MAKER_TAG == tag)
+            {
+                while(!queued_operations.empty())
+                {
+                    auto& op = queued_operations.front();
+                    queued_operations.pop_front();
+                    op->Complete(ok);
+                }
+            } else {
+                static_cast<TypeErasedOperation*>(tag)->Complete(ok);
+            }
+        }
+    }
+};
+
+struct GrpcContext::executor_type
+{
+    GrpcContext* grpc_context {};
+
+    template<class Function>
+    void execute(Function function) const
+    {
+        auto* op = allocate<Operation<Function>>(function);
+        grpc_context->queued_operations.push_front(op);
+        grpc_context->alarm.Set(grpc_context->queue.get(), gpr_time_0(GPR_CLOCK_REALTIME), GrpcContext::MAKER_TAG);
+    }
+};
+
+template<class CompletionToken>
+auto read(grpc::ClientAsyncReader<helloworld::HelloResponse>& reader,
+    helloworld::HelloResponse& response, CompletionToken token)
+{
+    return asio::async_initiate<CompletionToken, void(bool)>(
+        [&](auto completion_handler)
+        {
+            void* tag = allocate<Operation<decltype(completion_handler)>>(std::move(completion_handler));
+            reader.Read(&response, tag);
+        }, token);
+}
+
+asio::awaitable<void> ProcessRpc()
+{
+    std::unique_ptr<grpc::ClientAsyncReader<helloworld::HelloResponse>> reader;
+    helloworld::HelloResponse response;
+    co_await read(*reader, response, asio::use_awaitable);
+}
+
+int main() {
+    GrpcContext grpc_context{std::make_unique<grpc::CompletionQueue>()};
+    GrpcContext::executor_type exec{&grpc_context};
+    exec.execute([](bool){});
+    asio::co_spawn(exec, ProcessRpc(), asio::detached);
+    grpc_context.Run();
+}
