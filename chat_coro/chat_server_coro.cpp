65f78e9d7c2950c3de674e8abfca75f565a77f71
franktea <could.net@gmail.com>
2023-12-17 10:09:12 +0800

    add exception to remove clients

--- chat_coro/chat_server_coro.cpp
+++ chat_coro/chat_server_coro.cpp
@@ -33,6 +33,7 @@ public:
     // 玩家离开了聊天室
     void leave(chat_participant_ptr participant) {
         participants_.erase(participant);
+        std::cout << "remove client, now there a " << participants_.size() << " left\n";
     }
 
     // 向聊天室的所有玩家广播消息
@@ -68,29 +69,40 @@ public:
     // 读取消息，并放入当前聊天室的队列中以待广播
     asio::awaitable<void> read_msg() {
         auto self = shared_from_this();
-        while(1) {
-            // 读取消息，因为消息头部长度固定为4字节，只要先读4字节即可
-            co_await asio::async_read(socket_, asio::buffer(read_msg_.data(), read_msg_.header_length));
-            if(! read_msg_.decode_header()) { // 解析长度
-                std::cerr<<"decode header error, maybe msg too long.\n";
-                co_return;
-            }
-            // 读取到长度以后，再读取对应长度的消息内容
-            co_await asio::async_read(socket_, asio::buffer(read_msg_.body(), read_msg_.body_length()));
+        try {
+            while(1) {
+                // 读取消息，因为消息头部长度固定为4字节，只要先读4字节即可
+                co_await asio::async_read(socket_, asio::buffer(read_msg_.data(), read_msg_.header_length));
+                if(! read_msg_.decode_header()) { // 解析长度
+                    std::cerr<<"decode header error, maybe msg too long.\n";
+                    co_return;
+                }
+                // 读取到长度以后，再读取对应长度的消息内容
+                co_await asio::async_read(socket_, asio::buffer(read_msg_.body(), read_msg_.body_length()));
 
-            // 消息读取完了，放到chat_room里面去广播
-            room_.deliver(read_msg_);
+                // 消息读取完了，放到chat_room里面去广播
+                room_.deliver(read_msg_);
+            }
+        } catch (std::exception& e) {
+            std::cout<< "exception: " << e.what() << "\n";
+            socket_.close();
+            room_.leave(self);
         }
     }
 
     // 发送消息，将队列中的消息逐条发送
     asio::awaitable<void> send_mesgs() {
         auto self = shared_from_this();
-        // 只要write_msgs_不为空，这个协程就一定在运行
-        while(! write_msgs_.empty()) {
-            // 利用write_msgs_.front()来当buffer，只要没发送完，队列一定不为空
-            co_await asio::async_write(socket_, asio::buffer(write_msgs_.front().data(), write_msgs_.front().length()));
-            write_msgs_.pop_front(); // 发送完了才pop出来
+        try {
+            // 只要write_msgs_不为空，这个协程就一定在运行
+            while(! write_msgs_.empty()) {
+                // 利用write_msgs_.front()来当buffer，只要没发送完，队列一定不为空
+                co_await asio::async_write(socket_, asio::buffer(write_msgs_.front().data(), write_msgs_.front().length()));
+                write_msgs_.pop_front(); // 发送完了才pop出来
+            }
+        } catch (std::exception& e) {
+            socket_.close();
+            room_.leave(self);
         }
     }
 
